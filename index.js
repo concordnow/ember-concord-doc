24dc7610ae86bbfb131795cc831ced60c8e74e26
Camille TJHOA <camille.tjhoa@outlook.com>
2021-04-01 08:34:36 +0000

    Load tailwind only on dummy app

--- index.js
+++ index.js
@@ -2,4 +2,21 @@
 
 module.exports = {
   name: require('./package').name,
+
+  contentFor(type, config) {
+    if (
+      type === 'head-footer' &&
+      config.modulePrefix === 'dummy' &&
+      config.environment != 'test'
+    ) {
+      return [
+        'tailwindcss@^2.0/dist/base.min.css',
+        'tailwindcss@^2.0/dist/components.min.css',
+        '@tailwindcss/typography@^0.4/dist/typography.min.css',
+        'tailwindcss@^2.0/dist/utilities.min.css',
+      ]
+        .map((css) => `<link href="https://unpkg.com/${css}" rel="stylesheet">`)
+        .join('');
+    }
+  },
 };
