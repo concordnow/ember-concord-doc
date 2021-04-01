9d5c8645594d51d4ae2496b7d04b64b2979f0ddd
Camille TJHOA <camille.tjhoa@outlook.com>
2021-04-01 08:45:53 +0000

    Add prism, code-snippet, md-block dependencies

--- index.js
+++ index.js
@@ -19,4 +19,56 @@ module.exports = {
         .join('');
     }
   },
+
+  included(includer) {
+    if (includer.parent) {
+      throw new Error(
+        `ember-concord-doc should be in your package.json's devDependencies`
+      );
+    } else if (includer.name === this.project.name()) {
+      throw new Error(
+        `ember-concord-doc cannot be used to document an application`
+      );
+    }
+
+    if (!includer.options.snippetSearchPaths) {
+      includer.options.snippetSearchPaths = ['addon', 'tests/dummy/app'];
+    }
+    if (!includer.options['ember-prism']) {
+      includer.options['ember-prism'] = {};
+    }
+
+    if (!includer.options['ember-prism'].theme) {
+      includer.options['ember-prism'].theme = 'tomorrow';
+    }
+
+    if (!includer.options['ember-prism'].components) {
+      includer.options['ember-prism'].components = [
+        'markup',
+        'markup-templating',
+        'handlebars',
+        'javascript',
+        'css',
+      ];
+    }
+
+    if (!includer.options['ember-md-block']) {
+      includer.options['ember-md-block'] = {};
+    }
+
+    if (!includer.options['ember-md-block'].wrapper) {
+      includer.options['ember-md-block'].wrapper = {};
+    }
+
+    if (!includer.options['ember-md-block'].wrapper.begin) {
+      includer.options['ember-md-block'].wrapper.begin =
+        '<div class="prose max-w-none">';
+    }
+
+    if (!includer.options['ember-md-block'].wrapper.end) {
+      includer.options['ember-md-block'].wrapper.end = '</div>';
+    }
+
+    this._super.included.apply(this, arguments);
+  },
 };
